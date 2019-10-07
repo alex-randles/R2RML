2a7a8ca2f0ceaa98ef8e167d3a7237fab6859b5c
Alex <alex.randles2@mail.dcu.ie>
2019-10-07 10:38:38 +0100

    beta version 0.1 completed

--- API.py
+++ API.py
@@ -1,21 +1,17 @@
 # imported modules
 from flask import Flask, render_template, request, send_file, send_from_directory
-from mapping import get_column_names
-# api framework
-app = Flask(__name__)
-
-from flask_caching import Cache
 from werkzeug import secure_filename
+from mapping import create_output, get_column_names
 import os
-app = Flask(__name__)
 
+app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # to prevent caching of previous result
 file_counter = 0
 app.config["allowed_file_extensions"] = ["csv"]
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config["AUDIO_FOLDER"] =  "/home/alex/PycharmProjects/R2RML/static/audio_files"
+file_name = ""
 
 
 class API:
@@ -23,33 +19,42 @@ class API:
     def __init__(self):
         app.run(host="127.0.0.1", port="5000", threaded=True, debug=True)
 
+    @staticmethod
+    def create_output_file(data,filename):
+        output_file = open(filename, "w")
+        output_file.write(data)
+
     @app.route("/", methods=["GET", "POST"])
     def display_results():
         if request.method == "POST":
-            prefix_1 = request.values.get('prefix_1')
-            prefix_2 = request.values.get("prefix_2")
             # get file uploaded
             file = request.files['file']
-
             if file:
-                # will default to None
-
-                # save file
                 filename = secure_filename(file.filename)
                 file_extension = filename.split(".")[1]
-                print(file, file.filename)
-                print(file_extension)
-                # if file_extension in app.config["allowed_file_extensions"]:
+                global file_path
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                print("file path:", file_path)
                 saved_file = file.save(file_path)
-
-                prefixes = [prefix_1, prefix_2]
-                print("prefixes", prefixes)
-            return render_template("post.html", results=get_column_names(file_path), prefixes=prefixes)
+                return render_template("post.html", results=get_column_names(file_path))
+            else:
+                return "No file uploaded....."
         else:
             return render_template("get.html")
 
+    @app.route("/result", methods=["GET", "POST"])
+    def return_result():
+        subject_prefix = request.values.get('subject_prefix')
+        prefixes = []
+        global file_path
+        for i in range(0, len(get_column_names(file_path))):
+            prefixes.append(request.values.get("prefix_" + str(i)))
+        return create_output(file_path, "test_output.ttl", subject_prefix, prefixes)
+
+    @app.route("/download-file/", methods=["GET"])
+    def return_file():
+        print("Downloading file.......")
+        download_path = "test_output.ttl"
+        return send_file(download_path, as_attachment=True, cache_timeout=0)
 
 
 if __name__ == "__main__":
--- mapping.py
+++ mapping.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from flask import Flask, render_template, request, send_file, send_from_directory
+
 
 csv_file = "people.csv"
 
 
 def read_file(file_name):
     data = pd.read_csv(file_name)
-    print(data)
     return data
 
 
@@ -14,44 +15,36 @@ def get_column_names(file_name=csv_file):
     return column_names
 
 
-def create_output():
+def create_output(file_name, output_file_name,  subject_prefix, column_prefixes):
     output = """"""
-    # class_type = "<http://www.txample.com/people/voc/Person>"
-    class_type = input("What type of class is this data? ")
-    subject = input("What is the subject of the data? ")
-    subject_prefix = subject = subject[:-1] + "/{}" + subject[-1]
-    print(subject)
-    data = read_file("people.csv")
-    columns = data.columns.values
-    number_entries = len(data)
-    # subject = "<http://www.txample.com/people/voc/Person{}>\n".format(data[columns[0]][i])
-    # subject = input("What is the subject prefix?")
-    for i in range(0, 4):
-        columns = list(data.columns.values)
-        subject = subject_prefix.format(data[columns[0]][i]) + "\n"
-        # print(subject, data[columns[0]][i])
-        class_instance = "  a     {}   ;\n".format(class_type)
-        output += subject
+    data = read_file(file_name)
+    for i in range(0, len(data)):
+        columns = get_column_names(file_name)
+        output += subject_prefix.split(">")[0] + str(data[columns[0]][i])+ subject_prefix.split(">")[1] + ">" + "\n"
+        class_instance = "  a     {} ;\n".format(subject_prefix)
         output += class_instance
-        prefix = "  <http://www.txample.com/people/voc/{}>"
         for column in columns[1:]:
             current_value = data[column][i]
-            predicate = prefix.format(column) + "\n"
+            # predicate = prefix.format(column) + "\n"
+            index = columns.index(column) - 1
+            predicate = '  {}/{}>\n'.format(column_prefixes[index][:-1], column)
             if columns.index(column) == len(columns)-1:
                 object_value = '        "{}" {}\n'.format(current_value, ".")
             else:
                 object_value = '          "{}" {}\n'.format(current_value, ";")
             output += predicate
             output += object_value
-        write_file("output_file.ttl", output)
+    write_file(output_file_name, output)
+    return render_template("result.html", result=output)
 
 
 def write_file(output_file, text):
-    output_file = open("output_file.ttl", "w")
+    output_file = open(output_file, "w")
     output_file.write(text)
     print("Output written to {}.....".format(output_file))
 
 
 if __name__ == "__main__":
-    print(get_column_names())
-    # create_output()
+    # create_output("people.csv", "output.ttl", "<http://www.txample.com/people/voc/Person>", [ "<http://www.NAME.com/people/voc/Person/>", "<http://www.FIRST.com/people/voc/Person/>", "<http://www.LAST.com/people/voc/Person/>", "<http://www.MIDDLE.com/people/voc/Person/>", "<http://www.EMAIL.com/people/voc/Person/>", "<http://www.PHONE.com/people/voc/Person/>", "<http://www.FAX.com/people/voc/Person/>", "<http://www.TITLE.com/people/voc/Person/>"])
+    create_output("static/uploads/EMP.CSV", "test_output.ttl", '<http://www.txample.com/people/voc/Person>',  ['<http://www.txample.com/people/voc/Person>','<http://www.txample.com/people/voc/Person>','<http://www.txample.com/people/voc/Person>', '<http://www.txample.com/people/voc/Person>','<http://www.txample.com/people/voc/Person>'])
+
